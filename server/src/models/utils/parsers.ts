5f1f7ff2262b5b394de0c5dc075068cc96519739
TB516 <tberrios9800@gmail.com>
2024-10-09 18:34:58 -0400

    UrlEncoded parsing complete

--- server/src/models/utils/parsers.ts
+++ server/src/models/utils/parsers.ts
@@ -5,8 +5,32 @@ const parseJson = (content: string) : ISpell => {
 };
 
 const parseUrlencoded = (content: string): ISpell => {
-  // Look at demos
-  return {} as ISpell;
+  const decodedBody = new URLSearchParams(content);
+  const spell = {} as ISpell;
+
+  spell.name = decodedBody.get('name')!;
+  spell.image = decodedBody.get('image')!;
+  spell.description = decodedBody.get('description')!;
+  spell.type = decodedBody.get('type')!;
+  spell.cost = Number.parseInt(decodedBody.get('cost')!, 10);
+  spell.slots = Number.parseInt(decodedBody.get('slots')!, 10);
+  spell.effects = decodedBody.get('string')!;
+  spell.requires = [
+    {
+      name: 'Intelligence',
+      amount: Number.parseInt(decodedBody.get('intReq')!, 10),
+    },
+    {
+      name: 'Faith',
+      amount: Number.parseInt(decodedBody.get('faithReq')!, 10),
+    },
+    {
+      name: 'Arcane',
+      amount: Number.parseInt(decodedBody.get('arcReq')!, 10),
+    },
+  ];
+
+  return spell;
 };
 
 export { parseJson, parseUrlencoded };
