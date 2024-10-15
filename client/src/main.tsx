9961cdf5ddf936089a827c0b93ec7eb60d528127
TB516 <tberrios9800@gmail.com>
2024-10-14 20:12:52 -0400

    Created client framework

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Test</div>
+    element: <Navbar></Navbar>,
+    children: [
+
+    ]
   }
 ])
 
