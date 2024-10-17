6e7fb26f7b0bdd48f9ac824bd533553990e4da1e
TB516 <tberrios9800@gmail.com>
2024-10-16 20:49:42 -0400

    Added headers to client requests

--- client/src/services/SpellService.ts
+++ client/src/services/SpellService.ts
@@ -5,7 +5,10 @@ export abstract class SpellService{
   public static GetEndpointResponse = async (endpoint: string, args?: ISpellQuery, method: string = "GET") => {
     const searchParams = args? `name=${args.name}&cost=${Number.isNaN(args.cost) ? "" : args.cost}&slots=${Number.isNaN(args.slots) ? "" : args.slots}` : '';
     return await fetch(`${endpoint}?${searchParams}`, {
-      method: method
+      method: method,
+      headers: {
+        'accept': 'application/json'
+      }
     });
   };
 
@@ -14,6 +17,7 @@ export abstract class SpellService{
       body: JSON.stringify(spell),
       method: 'POST',
       headers:{
+        'accept': 'application/json',
         'content-type': 'application/json'
       }
     });
