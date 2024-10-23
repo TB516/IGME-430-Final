09a92aca3ad894cfa8b4256919f23f464e67ccd5
TB516 <tberrios9800@gmail.com>
2024-10-22 23:54:31 -0400

    Added favicon support and index file routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import compression from 'compression';
+import favicon from 'serve-favicon';
 import path from 'path';
 import apiRouter from './controllers/api';
 
@@ -10,17 +11,28 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const app = express();
 
 app.use(compression());
-
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.resolve(`${__dirname}/../client/`)));
-} else {
-  app.use('/', express.static(path.resolve(`${__dirname}/../../dist/client/`)));
-}
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../client/assets`)));
+  app.use(favicon(path.resolve(`${__dirname}/../client/favicon.png`)));
+} else {
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../../dist/client/assets`)));
+  app.use(favicon(path.resolve(`${__dirname}/../../dist/client/favicon.png`)));
+}
+
 app.use('/api', apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (_request: Request, response: Response) => {
+    response.sendFile(path.resolve(`${__dirname}/../client/index.html`));
+  });
+} else {
+  app.get('*', (_request: Request, response: Response) => {
+    response.sendFile(path.resolve(`${__dirname}/../../dist/client/index.html`));
+  });
+}
 
 mongoose.connect(process.env.MONGODB_URI!).catch((err) => { console.log(err); });
 
