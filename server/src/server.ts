09f61b32e8e2817fbb17065e64d2100051fa9194
TB516 <tberrios9800@gmail.com>
2024-11-09 04:42:48 -0500

    Updated server port access

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ import apiRouter from './controllers/api';
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 app.use(cors());
 app.use(compression());
