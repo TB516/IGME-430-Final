a7d9a61a9ffe6f3aab626669a99616d416940663
TB516 <tberrios9800@gmail.com>
2024-11-24 04:10:01 -0500

    Added client error controllers back

---
+++ api/src-new/controllers/error/client/badRequest.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import IErrorMessage from '../../../models/IErrorMessage';
+
+/**
+ * Responds with a bad request status and the entered message
+ * @param _request request object
+ * @param response response object
+ * @param message message for response body
+ */
+const badRequestResponse = (_request: Request, response: Response, message: IErrorMessage) => {
+  response.status(400).json(message);
+};
+
+export default badRequestResponse;
---
+++ api/src-new/controllers/error/client/resourceNotFound.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import IErrorMessage from '../../../models/IErrorMessage';
+
+/**
+ * Responds with a 404 resource not found status.
+ * @param _request request object
+ * @param response response object
+ * @param message message to send
+ */
+const resourceNotFoundResponse = (_request: Request, response: Response, message: IErrorMessage) => {
+  response.status(404).json(message);
+};
+
+/**
+ * Responds with a 404 endpoint not found status specifying that the requested API endpoint doesnt exist.
+ * @param request request object
+ * @param response response object
+ * @returns
+ */
+const endpointNotFoundResponse = (request: Request, response: Response) => {
+  return resourceNotFoundResponse(request, response, {
+    id: 'endpointNotFound',
+    message: `The requested endpoint of ${request.url} was not found.`,
+  } as IErrorMessage);
+};
+
+export default resourceNotFoundResponse;
+export { endpointNotFoundResponse };
