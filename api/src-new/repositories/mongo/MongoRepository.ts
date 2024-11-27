ec909f1d850ee4e5a039a89db5c1b5a957e2f16a
TB516 <tberrios9800@gmail.com>
2024-11-26 21:23:11 -0500

    Added base mongo repository

---
+++ api/src-new/repositories/mongo/MongoRepository.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import mongoose from 'mongoose';
+import IRepository from '../IRepository';
+import Data from '../../models/Data';
+import Query from '../../models/Query';
+
+abstract class MongoRepository<T extends Data> implements IRepository<T> {
+  protected connection: mongoose.Connection;
+
+  protected model: mongoose.Model<T>;
+
+  abstract search(query: Query): Promise<T[]>;
+
+  abstract findById(id: string): Promise<T | undefined>;
+
+  abstract exists(spell: T): Promise<boolean>;
+
+  abstract create(spell: T): Promise<boolean>;
+
+  abstract updateById(id: string, spell: T): Promise<T>;
+
+  abstract deleteById(id: string): Promise<T>;
+}
+
+export default MongoRepository;
