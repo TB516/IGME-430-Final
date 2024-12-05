ab9a8654ec7854da1fa64e703a31e6b1e623cd61
TB516 <tberrios9800@gmail.com>
2024-12-04 22:33:31 -0500

    added has auth middleware

---
+++ api/src/middleware/hasAuth.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import * as errors from '../utils/error/client';
+
+interface JWTBody {
+  isAdmin: boolean,
+}
+
+const hasAuth = (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const token = request.headers.authorization ? request.headers.authorization.replace('Bearer', '') : '';
+
+    const payload = verify(token, process.env.authSecret!) as JWTBody;
+
+    if (!payload.isAdmin) {
+      errors.unauthorizedResponse(request, response);
+      return;
+    }
+
+    next();
+  } catch {
+    errors.unauthorizedResponse(request, response);
+  }
+};
+
+export default hasAuth;
