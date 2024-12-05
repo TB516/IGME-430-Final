77c5f8aa22b6d7e396bc04330c4ca59494da7f4f
TB516 <tberrios9800@gmail.com>
2024-12-05 14:28:47 -0500

    Changed to just use token for verification

--- api/src/middleware/hasAuth.ts
+++ api/src/middleware/hasAuth.ts
@@ -2,20 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import * as errors from '../utils/error/client';
 
-interface JWTBody {
-  isAdmin: boolean,
-}
-
 const hasAuth = (request: Request, response: Response, next: NextFunction) => {
   try {
     const token = request.headers.authorization ? request.headers.authorization.replace('Bearer', '').trim() : '';
 
-    const payload = verify(token, process.env.AUTH_SECRET!) as JWTBody;
-
-    if (!payload.isAdmin) {
-      errors.unauthorizedResponse(request, response);
-      return;
-    }
+    verify(token, process.env.AUTH_SECRET!);
 
     next();
   } catch {
