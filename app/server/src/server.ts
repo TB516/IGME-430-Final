25afa7e117b43f6229c59097d9c7f6d5a3965d93
TB516 <tberrios9800@gmail.com>
2024-12-08 01:22:14 -0500

    Added cors package to server

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -1,6 +1,7 @@
 import compression from 'compression';
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 
 require('dotenv').config();
 
@@ -8,6 +9,7 @@ const app = express();
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 app.use(helmet());
+app.use(cors({ origin: process.env.CLIENT_DOMAIN }));
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
