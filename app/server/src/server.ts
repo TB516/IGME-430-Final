558c56727534fd6088235ae093a3094af954671e
TB516 <tberrios9800@gmail.com>
2024-12-09 06:02:56 -0500

    Adjusted cors call

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 const port = process.env.PORT || process.env.NODE_PORT || 3001;
 
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_DOMAIN }));
+app.use(cors());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
