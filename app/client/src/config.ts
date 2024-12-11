7b0396265ef713a1be03be5bd4d83faf6c2ed785
TB516 <tberrios9800@gmail.com>
2024-12-11 18:48:05 -0500

    Update config.ts

--- app/client/src/config.ts
+++ app/client/src/config.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = import.meta.env.SERVER_URL;
-const API_URL = import.meta.env.API_URL;
+const SERVER_URL = import.meta.env.SERVER_URL ? import.meta.env.SERVER_URL : 'http://localhost:3001';
+const API_URL = import.meta.env.API_URL ? import.meta.env.API_URL : 'http://localhost:3000';
 
 export { SERVER_URL, API_URL }
