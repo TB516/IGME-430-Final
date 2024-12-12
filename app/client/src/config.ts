782e9690c0b3235c8a5c45417409b07d663e7d34
TB516 <tberrios9800@gmail.com>
2024-12-11 19:29:55 -0500

    Fixed url spelling

--- app/client/src/config.ts
+++ app/client/src/config.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = import.meta.env.PROD ? 'https://igme-430-final-app-server-320ae5ec9b58.herokuapp.com/' : 'http://localhost:3001';
-const API_URL = import.meta.env.PROD ? 'https://igme-430-final-api-4431d83a3c44.herokuapp.com/' : 'http://localhost:3000';
+const SERVER_URL = import.meta.env.PROD ? 'https://igme-430-final-app-server-320ae5ec9b58.herokuapp.com' : 'http://localhost:3001';
+const API_URL = import.meta.env.PROD ? 'https://igme-430-final-api-4431d83a3c44.herokuapp.com' : 'http://localhost:3000';
 
 export { SERVER_URL, API_URL }
